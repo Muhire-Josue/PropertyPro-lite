5aff7471e2109321cf6bfb9f3d47accba6a9ce36
Muhire-Josue <muhirejosue09@gmail.com>
2019-06-24 18:06:51 +0000

    completed all endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const user = require("./model/user")
+let property = require("./model/property")
 const jwt = require("jsonwebtoken")
 //const auth = require("./middleware/auth")
 
@@ -8,35 +9,40 @@ app.use(express.json());
 
 
 //generates tokens
-const generateToken = (thisUser)=>{
+const generateToken = (thisUser) => {
     const token = jwt.sign({ thisUser }, "apisecretkey")
     return token
 }
 
 
-
-app.post("/signup/api/v1/", (req, res)=>{
+//User signup
+app.post("/signup/api/v1/", (req, res) => {
     const newUser = req.body;
     let token = generateToken(newUser);
     newUser.token = token
-    newUser.id = user.length +1;
+    newUser.id = user.length + 1;
     user.push(newUser);
-    res.send({status: "success", data: user, token: token})
+    res.send({ status: "success", data: user, token: token })
 });
 
+
+
 //User signin
-app.post("/signin/api/v1/", (req, res)=>{
-    const currentUser = user.find(u=>u.email === req.body.email)
-    if(currentUser.password === req.body.password){
-        let token = generateToken(currentUser)
-        res.send({status: "success", data: currentUser, token: token})
+app.post("/signin/api/v1/", (req, res) => {
+    const currentUser = user.find(u => u.email === req.body.email)
+    if (currentUser.password === req.body.password) {
+        let token = generateToken(currentUser);
+        res.send({ status: "success", data: currentUser, token: token });
     }
-    res.status(404).send({status: "error", error: "Could not find user"})
+    res.status(404).send({ status: "error", error: "Could not find user" });
 })
 
-app.get("/test/", verifyToken, (req, res)=>{
-    jwt.verify(req.token, "apisecretkey", (error, authData)=>{
-        if(error){
+
+
+//auth test
+app.get("/test/", verifyToken, (req, res) => {
+    jwt.verify(req.token, "apisecretkey", (error, authData) => {
+        if (error) {
             res.status(400).send("Unauthorized access");
         } else {
             res.send({
@@ -47,9 +53,84 @@ app.get("/test/", verifyToken, (req, res)=>{
     });
 });
 
-function verifyToken(req, res, next){
+
+
+
+//post property
+app.post("/property/api/v1/", verifyToken, (req, res) => {
+
+    jwt.verify(req.token, "apisecretkey", (error, authData) => {
+        if (error) {
+            res.status(400).send("Unauthorized access");
+        } else {
+            let newProperty = req.body
+            newProperty.id = property.length + 1;
+            newProperty.owner = authData.id;
+            property.push(newProperty);
+            res.send({ status: "success", data: newProperty })
+        }
+    });
+});
+
+
+
+//get all property
+app.get("/property/api/v1", (req, res) => {
+    
+    if(req.query.type){
+        property = property.filter(u => u.type === req.query.type);
+        res.send(property);
+    } else {
+        res.send(property);
+    }
+    
+});
+
+
+
+//update property
+app.patch('/property/:propertyid/api/v1', function (req, res) {
+    //const newProperty = property.find(u=>u.id === parseInt(req.params.propertyid));
+    const propertIndex = property.findIndex(u => u.id === parseInt(req.params.propertyid));
+    property[propertIndex].id = parseInt(req.params.propertyid);
+    property[propertIndex] = req.body;
+    res.send(property[propertIndex]);
+
+});
+
+
+//delete property
+app.delete("/property/:propertyid/api/v1", (req, res) => {
+    property = property.filter(function (obj) {
+        return obj.id !== parseInt(req.params.propertyid);
+    });
+    res.send(property)
+});
+
+
+//mark a property as sold
+app.patch("/property/:propertyid/sold/api/v1", (req, res) => {
+    const propertIndex = property.findIndex(u => u.id === parseInt(req.params.propertyid));
+    property[propertIndex].status = "sold";
+    res.send(property[propertIndex])
+});
+
+
+//get property by id
+app.get("/property/:propertyid/api/v1", (req, res)=>{
+    const propertIndex = property.findIndex(u => u.id === parseInt(req.params.propertyid));
+    res.send(property[propertIndex]);
+});
+
+
+
+
+
+
+//verify token
+function verifyToken(req, res, next) {
     const bearerHeader = req.headers['authorization'];
-    if(typeof bearerHeader !== undefined){
+    if (typeof bearerHeader !== undefined) {
 
         const bearer = bearerHeader.split(' ')
         const bearerToken = bearer[1];
@@ -62,8 +143,8 @@ function verifyToken(req, res, next){
 }
 
 
-app.listen(3001, ()=>{
+app.listen(3000, () => {
     // eslint-disable-next-line no-console
-    console.log("server listning on port 3001");
-    
+    console.log("server listning on port 3000");
+
 });
---
+++ src/model/property.js
@@ -0,0 +1,16 @@
+let property = [
+    {
+        id: 1,
+        owner: 1,
+        status: "available",
+        price: 1.2,
+        state: "Rwanda",
+        city: "Kigali",
+        address: "Kagugu",
+        type: "2bedroom",
+        image_url: "not found"
+
+    }
+];
+
+module.exports = property;
